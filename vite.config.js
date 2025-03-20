6f883861a3ac68ef1069a39536054d58267c6f07
TackTeam-Chare <chare.auppachitwan@gmail.com>
2025-03-20 17:26:23 +0700

    Fix: Allow Railway host in Vite preview

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default defineConfig({
+    preview: {
+        allowedHosts: ["boredtopiademogame-production.up.railway.app"],
+    },
+    server: {
+        host: true,
+        port: process.env.PORT || 4173,
+    },
+});
